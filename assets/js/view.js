c1f45a0ab3090eec888dd15566056835983c3201
unknown <kevinwalewangko078@gmail.com>
2020-12-14 20:38:46 +0800

    Menambah warna merah

--- assets/js/view.js
+++ assets/js/view.js
@@ -147,7 +147,7 @@ function AddPost(id, categoryID, creator, imageURL, title){
                                 <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
                                 </svg>
                             </button>
-                            <h4 id="vote-${id}"> 0</h4>
+                            <h4 style="background-color:#FF0000" id="vote-${id}"> 0</h4>
                         </div>
                         <div class="image">
                             <img src="${imageURL}">
